f960f4792c00f5e08f07938034c4f3f4f77a7227
dakson99 <darko99.mikavica@gmail.com>
2025-03-10 09:25:57 +0100

    Second work

--- Animation 2/app.js
+++ Animation 2/app.js
@@ -27,3 +27,66 @@ ScrollTrigger.create({
     pin: true,
     pinSpacing: false,
 });
+
+ScrollTrigger.create({
+    trigger: ".pinned",
+    start: "top top",
+    endTrigger: ".header-info",
+    end: "bottom bottom",
+    onUpdate: (self) => {
+        const rotation = self.progress * 360;
+        gsap.to(".revealer", { rotation })
+    }
+});
+
+ScrollTrigger.create({
+    trigger: ".pinned",
+    start: "top top",
+    endTrigger: ".header-info",
+    end: "bottom bottom",
+    onUpdate: (self) => {
+        const progress = self.progress;
+        const clipPath = `polygon(
+        ${45 - 45 * progress}% ${0 + 0 * progress}%,
+        ${55 + 45 * progress}% ${0 + 0 * progress}%,
+        ${55 + 45 * progress}% ${100 - 0 * progress}%,
+        ${45 - 45 * progress}% ${100 - 0 * progress}%
+        )`;
+        gsap.to(".revealer-1, .revealer-2", {
+            clipPath: clipPath,
+            ease: "none",
+            duration: 0,
+        });
+    },
+});
+
+ScrollTrigger.create({
+    trigger: ".header-info",
+    start: "top top",
+    end: "bottom 50%",
+    scrub: 1,
+    onUpdate: (self) => {
+        const progress = self.progress;
+        const left = 35 + 15 * progress;
+        gsap.to(".revealer", {
+            left: `${left}%`,
+            ease: "none",
+            duration: 0,
+        });
+    },
+});
+
+ScrollTrigger.create({
+    trigger: ".whitespace",
+    start: "top 50%",
+    end: "bottom bottom",
+    scrub: 0,
+    onUpdate: (self) => {
+        const scale = 1 + 12 * self.progress;
+        gsap.to(".revealer", {
+            scale: scale,
+            ease: "none",
+            duration: 0,
+        });
+    },
+});
